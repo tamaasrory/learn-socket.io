1c721b86f7ae0939ead7eb1bf8569add7e36ab6a
tamaasrory <tamaasrory@gmail.com>
2020-06-23 09:40:31 +0700

    add method to boardcast message

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Ahlan wa sahlan</h1>');
+    // res.send('<h1>Ahlan wa sahlan</h1>');
+    res.sendFile(__dirname + '/page/index.html');
+});
+
+io.on('connection', socket => {
+    // socket.on('chat message', (msg) => {
+    //     console.log('message: ' + msg);
+    // });
+
+    // socket.broadcast.emit('hi');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, () => {
